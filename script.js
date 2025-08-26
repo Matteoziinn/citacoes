a88db0ac72f025b8fe9baec055203278fb9cc72a
Matteo <matteoscaetano@gmail.com>
2025-08-26 18:04:31 -0300

    feat: Adicionando mais citações para a pagina

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const citacoes = [
   "A imaginação é mais importante que o conhecimento. - Albert Einstein",
   "A vida é o que acontece enquanto você está ocupado fazendo outros planos. - John Lennon",
-  "O sucesso é ir de fracasso em fracasso sem perder o entusiasmo. - Winston Churchill"
+  "O sucesso é ir de fracasso em fracasso sem perder o entusiasmo. - Winston Churchill",
+  "A persistência é o caminho do êxito. - Charles Chaplin",
+  "A felicidade não está em fazer o que a gente quer, e sim querer o que a gente faz. - Jean-Paul Sartre",
+  "Só se pode alcançar um grande êxito quando nos mantemos fiéis a nós mesmos. - Friedrich Nietzsche",
+  "O futuro pertence àqueles que acreditam na beleza de seus sonhos. - Eleanor Roosevelt",
+  "A coragem não é a ausência do medo; é a persistência apesar do medo. - Nelson Mandela"
 ];
+
 
 const p = document.querySelector('#citacao p');
 const botaoEl = document.getElementById("novaCitacaoBtn");
